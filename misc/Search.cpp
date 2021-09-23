27cb95d562b7a702784a4e5a90e9551d5d5afb67
Namus Shukra <82902672+Namus79@users.noreply.github.com>
2021-09-23 14:51:29 +0300

    edited search function

--- misc/Search.cpp
+++ misc/Search.cpp
@@ -1,98 +1,49 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-unsigned short int emp_Id;
-string firstName, middleName, lastName;     //full name
-string phoneNo, email, country, city, poBox;    //address
-string gender;
-unsigned short int age;
-string eduLevel;
-string department;
-float initialSalary, currentSalary;     //salary
-int dOfEmployment, mOfEmployment, yOfEmployment;     //date of employment
-string paymentType;     //hourly, weekly, monthly
-unsigned short int experianceLevel;     //measured in years
-string typeOfEmployee;      //fixed or contractual
-
-//Payroll Details
-float paymentPer;
-float overtimeHours;
-float grossPay;
-float tax;
-float pension;
-float netPay;
-
-unsigned numEmployee{};
-
-struct employee{
-    string firstName, middleName, lastName;
-    unsigned short int emp_Id;
-};
-
-void searchbyName(employee lists[]){
+void searchbyName()     //search by name
+{
     string name1,name2,name3;
-    cout<<"ENTER THE NAME OF EMPLOYEE YOU WANT TO FIND: "<<"\n";
-    cin>>name1>>name2>>name3;
-    int num=0;
-    for (int i{}; i<numEmployee; i++){
-    if(name1==lists[i].firstName||name2==lists[i].middleName||name3==lists[i].lastName) {
-            num++;
-    }
-    }
-
-    if(num==0)
+    cout<<"ENTER THE FULL NAME OF EMPLOYEE YOU WANT TO FIND: "<<"\n";
+    cout<<"First Name: ";
+    cin>>name1;
+    cout<<"Middle Name: ";
+    cin>>name2;
+    cout<<"Last Name: ";
+    cin>>name3;
+    for(int i = 0; i<name1.length(); i++)
+        name1.at(i) = char(tolower(name1.at(i)));
+    for(int i = 0; i<name2.length(); i++)
+        name2.at(i) = char(tolower(name2.at(i)));
+    for(int i = 0; i<name3.length(); i++)
+        name3.at(i) = char(tolower(name3.at(i)));
+    for (int i{}; i<employee.size(); i++)
     {
-        cout<<"\n"<<"EMPLOYEE NOT FOUND."<<"\n";
+        string fName=employee[i].fullName.firstName, mName=employee[i].fullName.middleName, lName=employee[i].fullName.lastName;
+        for(int i = 0; i<fName.length(); i++)
+            fName.at(i) = char(tolower(fName.at(i)));
+        for(int i = 0; i<mName.length(); i++)
+            mName.at(i) = char(tolower(mName.at(i)));
+        for(int i = 0; i<lName.length(); i++)
+            lName.at(i) = char(tolower(lName.at(i)));
+        if(name1==fName && name2==mName && name3==lName)
+        {
+            current_employee(1,i);
+            return;
+        }
     }
-    else
-    {
-        cout<<"\n"<<"EMPLOYEE FOUND."<<"\n";
-    }
+    cout<<"\n"<<"EMPLOYEE NOT FOUND."<<"\n";
 }
 
-void searchbyId(employee lists[]){
+void searchbyId()      //search by ID
+{
     unsigned short int idNum;
     cout<<"ENTER THE ID OF EMPLOYEE YOU WANT TO FIND: "<<"\n";
     cin>>idNum;
-    int num=0;
-    for (int i{}; i<numEmployee; i++){
-    if(lists[i].emp_Id==idNum){
-            num++;
-    }
-    }
-
-    if(num==0)
+    for (int i=0; i<employee.size(); i++)
     {
-        cout<<"\n"<<"EMPLOYEE NOT FOUND."<<"\n";
+        if(employee[i].emp_Id==idNum)
+        {
+            current_employee(1, i);
+            return;
+        }
     }
-    else
-    {
-        cout<<"\n"<<"EMPLOYEE FOUND."<<"\n";
-    }
-}
-
-int main(){
-    unsigned employeeNum=0;
-    cout<<"ENTER THE NUMBER OF EMPLOYEES: ";
-    cin>>employeeNum;
-    numEmployee=employeeNum;
-    employee persons[employeeNum]={"Namus","Shukra","Hulkero",01};
-    cout<<"\n"<<setw(12)<<":::::SEARCH MENU:::::"<<setw(12)<<"\n";
-    cout<<"\n"<<setw(12)<<"CHOOSE SEARCHING METHODS"<<setw(12)<<"\n";
-    int choice;
-    cout<<"1.SEARCH BY NAME."<<"\n";
-    cout<<"2.SEARCH BY ID."<<"\n";
-    cin>>choice;
-    switch (choice){
-    case 1:searchbyName(persons);
-        break;
-
-    case 2:searchbyId(persons);
-        break;
-
-    default:
-        cout<<"INVALID CHOICE."<<"\n";
-        break;
-    }
-    return 0;
+    cout<<"\n"<<"EMPLOYEE NOT FOUND."<<"\n";
 }
